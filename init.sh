01f2c6f3c7133f6e6d4c5092459f643bbcee4dd0
Mikko Syrjalahti <mikko.syrjalahti@gmail.com>
2018-10-22 11:18:04 +0300

    Added HP GPIB dongle firmware

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
-sudo fxload -t fx2 -D /dev/bus/usb/002/002 -I  hp-gpib/gpib_firmware-2008-08-10/agilent_82357a/measat_releaseX1.8.hex
+#!/bin/bash
+
+fxload -t fx2 -D /dev/bus/usb/002/002 -I  fw/agilent_82357a/measat_releaseX1.8.hex
 sleep 3
-sudo fxload -t fx2 -D /dev/bus/usb/002/003 -I  hp-gpib/gpib_firmware-2008-08-10/agilent_82357a/measat_releaseX1.8.hex
-sudo gpib_config
+
+fxload -t fx2 -D /dev/bus/usb/002/003 -I  fw/agilent_82357a/measat_releaseX1.8.hex
+
+gpib_config
 sleep 2
-sudo chgrp gpib /dev/gpib*
-sudo chmod 660 /dev/gpib*
+
+chgrp gpib /dev/gpib*
+chmod 660 /dev/gpib*
